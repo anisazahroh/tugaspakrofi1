f7e92a856d04f0350e6183ffff6001137d3f93ad
anisazahroh <anisazahrotulw077@gmail.com>
2023-10-23 10:56:52 +0700

    membuat 3 screen layar

---
+++ src/About/Minuman.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Minuman = () => {
+  return (
+    <View>
+      <Text>Minuman</Text>
+    </View>
+  )
+}
+
+export default Minuman
+
+const styles = StyleSheet.create({})
---
+++ src/Snack/Camilan.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Camilan = () => {
+  return (
+    <View>
+      <Text>Camilan</Text>
+    </View>
+  )
+}
+
+export default Camilan
+
+const styles = StyleSheet.create({})
